ec4fdc01617cf4dd6eaafd30216125cf2ab8509e
ahmdIbrhm <ahmadalibrahim28@gmail.com>
2019-11-15 17:19:09 +0100

    Added blog

---
+++ Bicycle_Website/uploadRDF.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+start=`date +%s%3N`
+curl -X POST \
+     -H Content-Type:text/n3 \
+     -T $1 \
+     -G http://localhost:3030/test/data \
+     -d default
+end=`date +%s%3N`
+runtime=$((end-start))
+echo "Loading time: $runtime"
+
+
+
+
